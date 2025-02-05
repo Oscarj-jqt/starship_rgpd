2361a1631e3e34215a9fd6ae6fd1b79e70da57b3
Oscarj-jqt <jacquetoscar0@gmail.com>
2025-02-05 21:55:51 +0100

    Notes to MainController

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -10,9 +10,11 @@ use Symfony\Component\Routing\Attribute\Route;
 //It must have to match with the name of the file
 class MainController
 {
+    #It redirects the user(request) to the route he wants to go
     #[Route('/')]
     public function homepage()
    {
+       # Here the response (dev) of the request with a status (200 Ok or 404 error...)
         return new Response('Application <strong>symfony</strong>');
    }
 }
