c0165eca8474f0ac7820ba36303afba93c7bb62a
Oscarj-jqt <jacquetoscar0@gmail.com>
2025-02-05 22:12:38 +0100

    adding twig template

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 // It must have to match with the name of the file
-class MainController
+class MainController extends AbstractController
 {
     // It redirects the user(request) to the route he wants to go
     #[Route('/')]
-    public function homepage()
+    public function homepage(): Response
     {
+
         // Here the response (dev) of the request with a status (200 Ok or 404 error...)
-        return new Response('Application <strong>symfony</strong>');
+        //twig redirect a file matching with the method name (homepage here)
+        return $this->render('main/homepage.html.twig');
     }
 }
