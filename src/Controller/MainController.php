d9c92554712b126135e206a40adc3a794ca8100c
Oscarj-jqt <jacquetoscar0@gmail.com>
2025-02-05 22:01:16 +0100

    the package fixed MainController style to symfony convention

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,19 +2,17 @@
 
 namespace App\Controller;
 
-
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-
-//It must have to match with the name of the file
+// It must have to match with the name of the file
 class MainController
 {
-    #It redirects the user(request) to the route he wants to go
+    // It redirects the user(request) to the route he wants to go
     #[Route('/')]
     public function homepage()
-   {
-       # Here the response (dev) of the request with a status (200 Ok or 404 error...)
+    {
+        // Here the response (dev) of the request with a status (200 Ok or 404 error...)
         return new Response('Application <strong>symfony</strong>');
-   }
+    }
 }
