f65dc6cf7a8dcea7800ff841279886298bb0b2b2
Oscarj-jqt <jacquetoscar0@gmail.com>
2025-02-07 10:23:46 +0100

    Define security role in config

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -19,36 +19,45 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
 
+        // Création des utilisateurs (Admins, Pilotes, Customers)
+        $admin = new User();
+        $admin->setEmail('admin@starship.com');
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin->setPassword($this->passwordHasher->hashPassword($admin, 'adminpass'));
+        $manager->persist($admin);
+
+        $pilot1 = new User();
+        $pilot1->setEmail('pilot1@starship.com');
+        $pilot1->setRoles(['ROLE_PILOT']);
+        $pilot1->setPassword($this->passwordHasher->hashPassword($pilot1, 'pilotpass'));
+        $manager->persist($pilot1);
+
+        $pilot2 = new User();
+        $pilot2->setEmail('pilot2@starship.com');
+        $pilot2->setRoles(['ROLE_PILOT']);
+        $pilot2->setPassword($this->passwordHasher->hashPassword($pilot2, 'pilotpass'));
+        $manager->persist($pilot2);
+
+        $customer = new User();
+        $customer->setEmail('customer@starship.com');
+        $customer->setRoles(['ROLE_CUSTOMER']);
+        $customer->setPassword($this->passwordHasher->hashPassword($customer, 'customerpass'));
+        $manager->persist($customer);
+
+
         $ships = [
-            ['name' => 'Starship 1', 'class' => 'Explorer', 'captain' => 'Jean-Luc Pickles', 'status' => 'completed'],
-            ['name' => 'Starship 2', 'class' => 'Destroyer', 'captain' => 'Kathryn Journeyway', 'status' => 'in_progress'],
-            ['name' => 'Starship 3', 'class' => 'Cruiser', 'captain' => 'James T. Quick', 'status' => 'waiting'],
+            ['name' => 'Starship 1', 'class' => 'Explorer', 'captain' => $pilot1, 'status' => 'completed'],
+            ['name' => 'Starship 2', 'class' => 'Destroyer', 'captain' => $pilot2, 'status' => 'in_progress'],
+            ['name' => 'Starship 3', 'class' => 'Cruiser', 'captain' => $pilot1, 'status' => 'waiting'],
         ];
 
         foreach ($ships as $shipData) {
             $starship = new Starship();
             $starship->setName($shipData['name']);
             $starship->setClass($shipData['class']);
-            $starship->setCaptain($shipData['captain']);
+            $starship->setCaptain($shipData['captain']); // Assignation d'un pilote
             $starship->setStatus($shipData['status']);
-
             $manager->persist($starship);
-        }
-
-        $usersData = [
-            ['email' => 'admin@starship.com', 'roles' => ['ROLE_ADMIN'], 'password' => 'admin123'],
-            ['email' => 'pilot@starship.com', 'roles' => ['ROLE_PILOT'], 'password' => 'pilot123'],
-            ['email' => 'customer@starship.com', 'roles' => ['ROLE_CUSTOMER'], 'password' => 'customer123'],
-            ['email' => 'dpo@starship.com', 'roles' => ['ROLE_SPACE_CONTROLLER'], 'password' => 'dpo123'],
-        ];
-
-        foreach ($usersData as $userData) {
-            $user = new User();
-            $user->setEmail($userData['email']);
-            $user->setRoles($userData['roles']);
-            $hashedPassword = $this->passwordHasher->hashPassword($user, $userData['password']);
-            $user->setPassword($hashedPassword);
-            $manager->persist($user);
         }
 
 
