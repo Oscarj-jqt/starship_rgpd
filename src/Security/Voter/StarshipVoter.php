5352bd487d40ca02dde90e96c7b7cb64abde3b55
Oscarj-jqt <jacquetoscar0@gmail.com>
2025-02-07 10:25:30 +0100

    Add access depending on roles

--- src/Security/Voter/StarshipVoter.php
+++ src/Security/Voter/StarshipVoter.php
@@ -2,7 +2,43 @@
 
 namespace App\Security\Voter;
 
-class StarshipVoter
-{
+use App\Entity\Starship;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 
+class StarshipVoter extends Voter
+{
+    protected function supports(string $attribute, $subject): bool
+    {
+        return $attribute === 'VIEW' && $subject instanceof Starship;
+    }
+
+    protected function voteOnAttribute(string $attribute, $subject, TokenInterface $token): bool
+    {
+        $user = $token->getUser();
+        if (!$user instanceof User) {
+            return false;
+        }
+
+        // Récupérer le vaisseau
+        $starship = $subject;
+
+        // Si l'utilisateur est admin, il peut voir tous les vaisseaux
+        if (in_array('admin', $user->getRoles())) {
+            return true;
+        }
+
+        // Si l'utilisateur est un pilote et est assigné à ce vaisseau
+        if (in_array('pilot', $user->getRoles())) {
+            return $starship->getCaptain() === $user->getUsername();
+        }
+
+        // Si l'utilisateur est un client et est assigné à ce vaisseau
+        if (in_array('customer', $user->getRoles())) {
+            return $starship->getCaptain() === $user->getUsername();
+        }
+
+        return false;
+    }
 }
